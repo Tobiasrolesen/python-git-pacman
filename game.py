617f025fbde9c5f93c3168780f0126e9aa9277a6
tobiasrolesen <tobiasrolesen@gmail.com>
2022-10-12 13:46:57 +0200

    Ghost added as white square

--- game.py
+++ game.py
@@ -22,6 +22,13 @@ for i in range(6):
     img = pg.transform.scale(img, (32,32))
     pacman_images.append(img)
 
+ghost_images = []
+
+for m in range(2):
+    img = pg.image.load(f"images/GHOSTY_{m}.png")
+    img = pg.transform.scale(img, (32,32))
+    ghost_images.append(img)
+
 
 ## Level ##
 level = []
@@ -35,6 +42,10 @@ with open('level.txt', 'r') as level_file:
                 y = r*32
                 x = c*32
                 row.append(" ")
+            elif char == "g":
+                x1 = r*32
+                y1 = c*32
+                row.append(" ")
             else:
                 row.append(" ")
 
@@ -42,7 +53,6 @@ with open('level.txt', 'r') as level_file:
 
 num_rows = len(level)
 num_cols = len(level[0])
-
 
 
 
@@ -71,6 +81,7 @@ while running:
             elif event.key == pg.K_SPACE:
                 running = False
 
+
     # Move
     if direction == "left":
         x = x - 5
@@ -80,6 +91,18 @@ while running:
         y = y - 5
     elif direction == "down":
         y = y + 5
+
+    ghostmove = (random.randint(1,10))
+
+    if ghostmove == (1 or 2):
+        x1 = x1 - 5
+    elif ghostmove == (3 or 4):
+        x1 = x1 + 5
+    elif ghostmove == (5 or 6):
+        y1 = y1 - 5
+    elif ghostmove == (7 or 8):
+        y1 = y1 + 5
+
 
     # Draw level #
     screen.fill((0,0,0))
@@ -104,6 +127,20 @@ while running:
         screen.blit(pg.transform.rotate(pacman_images[r],90), (x, y))
     else:
         screen.blit(pacman_images[0], (x, y))
+
+    # Draw Ghost
+
+    g = int((tick/2)%2)
+    if direction == "left":
+        screen.blit(ghost_images[g], (x1, y1))
+    elif direction == "right":
+        screen.blit(pg.transform.rotate(ghost_images[g],180), (x1, y1))
+    elif direction == "up":
+        screen.blit(pg.transform.rotate(ghost_images[g],-90), (x1, y1))
+    elif direction == "down":
+        screen.blit(pg.transform.rotate(ghost_images[g],90), (x1, y1))
+    else:
+        screen.blit(ghost_images[0], (x1, y1))
             
 
     # Update screen
